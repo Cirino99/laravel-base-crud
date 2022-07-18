7cfd601a906b836ec8f6ef26b22f6075e98e07cc
Simone <cirino.simone99@gmail.com>
2022-07-18 16:48:51 +0200

    prima stampa di dati nella home

--- app/Http/Controllers/ComicsController.php
+++ app/Http/Controllers/ComicsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Comic;
+
 class ComicsController extends Controller
 {
     /**
@@ -13,7 +15,8 @@ class ComicsController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $comics = Comic::all();
+        return view('home', compact('comics'));
     }
 
     /**
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,5 +5,12 @@
 @section('mainContent')
 <main> 
     <h1>Sono la home</h1>
+    <ul>
+        @foreach ($comics as $comic)
+            <li>
+                {{$comic->title}} -- {{$comic->price}} -- {{$comic->sale_date}}
+            </li>
+        @endforeach
+    </ul>
 </main>
 @endsection
